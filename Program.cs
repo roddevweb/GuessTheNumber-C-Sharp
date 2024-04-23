473487fa51895629ef4758866f43c2a7ab7eca7d
rcoelhoweb <rcoelhoweb@gmail.com>
2024-04-23 12:08:16 -0400

    Guess the number

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessTheNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random random = new Random();
+            int secretNumber = random.Next(1, 101);
+
+            while (true)
+            {
+
+            Console.WriteLine("Devinez le nombre mystère entre 1 e 100 :");
+            int guess = Convert.ToInt32(Console.ReadLine());
+
+            if (guess == secretNumber)
+            {
+                Console.WriteLine("Bravo! Vous avez deviné le nombre mystère.");
+                break; //Sortir de la boucle si le nombre est trouvé
+            }
+            
+            else if (guess < secretNumber)
+            {
+                Console.WriteLine("Le nombre mystère est plus grand.");
+            }
+            
+            else
+            {
+                Console.WriteLine("Le nombre mystère est plus petit.");   
+
+                Console.ReadLine();             
+            }
+        }
+        }
+    }
+}
+
+        
+
+
+    
+    
